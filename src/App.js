e8884b1243f712d11bb866cc7b162abe78758613
Nikunj Parasar <nikunj.parasar@gmail.com>
2023-02-23 15:02:06 -0800

    simple animation, gradient, and screen compatibility implementation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {Footer, Blog, Possibility, Features, WhatAPEX, Header} from './containers';
 import {CTA, Brand, Navbar} from './components';
-
+import './App.css';
 const App = () => {
   return (
     <div className='App'>
