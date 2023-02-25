a41d0ba0ddc3ab366a834a46688f61346ecdc102
Nikunj Parasar <nikunj.parasar@gmail.com>
2023-02-25 01:42:53 -0800

    FIXED react tilt effect render issues

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import VanillaTilt from 'vanilla-tilt';
+
 import './header.css';
 // import people from '../../assets/people.png';
 import track from '../../assets/track.png'
-import tilt from './vanilla-tilt.js';
+
+function Tilt(props) {
+  const { options, ...rest } = props;
+  const tilt = useRef(null);
+
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, options);
+  }, [options]);
+
+  return <div ref={tilt} {...rest} />;
+}
+
+const options = {
+  reverse:           false,  // reverse the tilt direction
+  max:               20,     // max tilt rotation (degrees)
+  perspective:       1000,   // Transform perspective, the lower the more extreme the tilt gets.
+  scale:             1,      // 2 = 200%, 1.5 = 150%, etc..
+  speed:             200,    // Speed of the enter/exit transition
+  transition:        true,   // Set a transition on enter/exit.
+  axis:              null,   // What axis should be disabled. Can be X or Y.
+  reset:             true,   // If the tilt effect has to be reset on exit.
+  easing:            "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  glare:             true,   // if it should have a "glare" effect
+  "max-glare":       0.6,      // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+  "glare-prerender": false   // false = VanillaTilt creates the glare elements for you, otherwise
+                             // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
+};
 
 const Header = () => {
   return (
     <div className='apex__header section__padding' id='home'>
       <div className='apex__header-content'>
-        <h1 className='gradient__text'>Performance Perfected.  <br/>AI vehicle dynamics and driving analysis.</h1>
+        <h1 className='gradient__text'>Performance Perfected.  <br/>AI dynamics and driving analysis.</h1>
         <p>"If you’re going to push a piece of machinery to the limit, and expect to hold it together, you have to have 
           some sense of where that limit is. Look out there. Out there is the perfect lap. No mistakes, every gear change, every corner. Perfect."  
           <br/><br/>- Ken Miles, Ford v. Ferrari</p>
@@ -16,19 +44,14 @@ const Header = () => {
           <input type='email' placeholder='Your email adress'/>
           <button type="button">Get Started</button>
         </div>
-
-        {/* <div className='apex__header-content__people'>
-          <img src={people} alt = 'people'/>
-          <p>1600 people requested access</p>
-        </div> */}
       </div>
-      <div className='apex__header-image'>
-        <script src={tilt}></script>
-        <div className='tiltcard' data-tilt data-tilt-glare>
-          <img src={track} alt='track'/>
-        </div>
-        
-       
+      
+      <div className='apex__header-image'>  
+        <Tilt options={options}>
+          <div className='tiltcard'>
+            <img src={track} alt='track'/>
+          </div> 
+        </Tilt>
       </div>
     </div>
   )
